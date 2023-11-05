db2c40ba2ef85614004e8ce184340f94b7c687c8
Razaq Khan Mohammad Abdul <mohammadabdul.r@northeastern.edu>
2023-11-05 01:44:28 +0000

    Update plots_1.py

--- LAB4/src/analysis/plots_1.py
+++ LAB4/src/analysis/plots_1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from bagpy import bagreader
@@ -11,6 +11,7 @@ import matplotlib.patches as mpatches
 import math
 from scipy import signal
 from scipy import integrate
+from scipy.signal import butter, filtfilt
 import os
 
 # Check if the directory exists
@@ -45,9 +46,8 @@ northings = gps_data.Utm_northing - north_offset
 fig, ax = plt.subplots(1, 1)
 
 plt.scatter(eastings, northings, label='UTM data', s=10)
-plt.plot(0, 0, 'ro', label='Average UTM location', markersize=10)
 ax.annotate('Easting/Northing offset: {:.0f}m, {:.0f}m'.format(east_offset, north_offset),
-            xy=(-170, 30), fontsize='x-large',
+            xy=(-170, 30), fontsize='large',
             bbox=dict(boxstyle="round", fc="0.7", alpha=0.7))
 
 plt.xlabel('Easting [m]')
@@ -57,49 +57,7 @@ plt.title('GPS Easting and Northing data, offset by average value')
 plt.grid(True)
 plt.legend()
 plt.savefig('circlePics/Full_gps')
-# plt.show()
-
-
-# Plot calibration circles
-print("\nCreating GPS circles plot")
-fig, ax = plt.subplots(1, 1)
-
-east_offset = gps_data.Utm_easting[51:105].mean()
-north_offset = gps_data.Utm_northing[51:105].mean()
-
-eastings = gps_data.Utm_easting[51:105] - east_offset
-northings = gps_data.Utm_northing[51:105] - north_offset
-
-plt.scatter(eastings, northings, label='UTM data', s=70)
-plt.plot(0, 0, 'ro', label='Average UTM location', markersize=10)
-
-plt.xlabel('Easting [m]')
-plt.ylabel('Northing [m]')
-plt.title('GPS Easting and Northing data, offset by average value')
-
-plt.grid(True)
-plt.legend()
-plt.axis('equal')
-plt.savefig('circlePics/Calibration_circles_gps')
-# plt.show()
-
-
-# Plot quaternions
-print("\nCreating quaternions plot")
-fig, ax = plt.subplots(1, 1)
-
-orientations = ["IMU.orientation.w", "IMU.orientation.x", "IMU.orientation.y", "IMU.orientation.z"]
-for i in orientations:
-    plt.plot(imu_data[i], label=i)
-plt.title('Quaternion values')
-plt.xlabel('Sample Number')
-plt.ylabel('Value')
-
-plt.grid(True)
-plt.legend(fontsize=16)
-plt.savefig('circlePics/Orientations')
-# plt.show()
-
+plt.show()
 
 # Plot Linear Acceleration
 print("\nCreating Linear Acceleration plot")
@@ -108,7 +66,7 @@ linear_accelerations = ["IMU.linear_acceleration.x", "IMU.linear_acceleration.y"
 for i in linear_accelerations:
     mean = imu_data[i].mean()
     print("{}: Mean = {}".format(i, mean))
-    plt.plot(imu_data[i], label=i)
+    plt.plot(imu_data[i],linewidth=1, label=i)
 plt.title('Linear Acceleration values')
 plt.ylabel(r'Acceleration [$m/s^2$]')
 plt.xlabel('Sample Number')
@@ -117,18 +75,17 @@ plt.grid(True)
 plt.legend(fontsize=18)
 plt.tight_layout()
 plt.savefig('circlePics/Linear Accelerations')
-# plt.show()
+plt.show()
 
 # Plot Angular Velocities
 print("\nCreating Angular Velocity plot")
 fig, ax = plt.subplots(1, 1)
-# Ignore first 200 data points
 angular_velocities = ["IMU.angular_velocity.x", "IMU.angular_velocity.y", "IMU.angular_velocity.z"]
 print("\nAngular Velocity Data:")
 for i in angular_velocities:
     mean = imu_data[i].mean()
     print("{}: Mean = {}".format(i, mean))
-    plt.plot(imu_data[i], label=i, alpha=0.8)
+    plt.plot(imu_data[i],linewidth=1, label=i, alpha=0.8)
 plt.title('Angular Velocity values')
 plt.ylabel('Angular velocity [rad/s]')
 plt.xlabel('Sample Number')
@@ -137,31 +94,30 @@ plt.grid(True)
 plt.legend(fontsize=18)
 plt.tight_layout()
 plt.savefig('circlePics/Angular Velocities')
-# plt.show()
+plt.show()
 
 # Plot Magnetometers
 print("\nCreating Magnetometer plot")
 fig, ax = plt.subplots(1, 1)
 magnetic = ["MagField.magnetic_field.x", "MagField.magnetic_field.y", "MagField.magnetic_field.z"]
 for i in magnetic:
-    plt.plot(mag_data[i][200:]*1000, label=i)
+    plt.plot(mag_data[i][800:]*1000, label=i)
 plt.title('Magnetic Field Strength values')
 plt.ylabel('milli Gauss')
 plt.xlabel('Sample Number')
-
 plt.grid(True)
 plt.legend(fontsize=18)
 plt.tight_layout()
 plt.savefig('circlePics/Magnetometer')
-#plt.show()
+plt.show()
 
 # Plot Mag Calibration circles
 print("\nPlotting magnetometer calibration circles")
 fig, ax = plt.subplots(1, 1)
-magx = mag_data["MagField.magnetic_field.x"][1815:4365]*1000
-magy = mag_data["MagField.magnetic_field.y"][1815:4365]*1000
+magx = mag_data["MagField.magnetic_field.x"][1000:4000]*1000
+magy = mag_data["MagField.magnetic_field.y"][1000:4000]*1000
 
-plt.plot(magx, magy, label='Magnetometer Values')
+plt.scatter(magx, magy, s=10, label='Magnetometer Values')
 
 x_offset = (magx.max() + magx.min()) / 2
 y_offset = (magy.max() + magy.min()) / 2
@@ -187,8 +143,8 @@ plt.axvline(c='r', alpha=0.5)
 
 plt.plot(hix_x, hix_y, 'g')
 plt.plot(hiy_x, hiy_y, 'g')
-plt.text(10, 310, 'Y-offset: {:.1f}'.format(y_offset), fontsize='xx-large')
-plt.text(-50, 640, 'X-offset: {:.2f}'.format(x_offset), fontsize='xx-large')
+plt.text(10, 310, 'Y-offset: {:.1f}'.format(y_offset), fontsize='x-large')
+plt.text(-50, 640, 'X-offset: {:.2f}'.format(x_offset), fontsize='x-large')
 
 plt.plot(0, 0, 'ro', label='Origin', markersize=15)
 plt.plot(x_offset, y_offset, 'go', label='Center of Mag data', markersize=10)
@@ -201,7 +157,7 @@ plt.grid(True)
 plt.axis('equal')
 plt.legend()
 plt.savefig("circlePics/Mag calibration raw values")
-# plt.show()
+plt.show()
 
 
 # Plot Mag Hard Iron correction
@@ -228,7 +184,7 @@ pac = mpatches.Arc((0, 0), 180, 180, theta1=0.0, theta2=theta_deg,
 ax.add_patch(pac)
 
 
-plt.plot(magx, magy, label='Hard Iron calibrated Mag data')
+plt.scatter(magx, magy, s=10, label='Hard Iron calibrated Mag data')
 plt.plot(0, 0, 'ro', label='Origin', markersize=15)
 plt.ylabel('My [milli Gauss]')
 plt.xlabel('Mx [milli Gauss]')
@@ -238,7 +194,7 @@ plt.legend(loc='upper right')
 plt.grid(True)
 plt.axis('equal')
 plt.savefig("circlePics/Mag HI calibrated")
-# plt.show()
+plt.show()
 
 
 # Plot Mag Hard and Soft Iron correction
@@ -262,8 +218,8 @@ print("Scale Factor (minor/major axis length): {}".format(scale_factor))
 soft_iron_corrected_x = theta_corrected_data[0]*scale_factor
 soft_iron_corrected_y = theta_corrected_data[1]
 
-plt.plot(magx, magy, label='Original')
-plt.plot(soft_iron_corrected_x, soft_iron_corrected_y, label='HSI calibrated')
+plt.scatter(magx, magy,s=10, label='Original')
+plt.scatter(soft_iron_corrected_x, soft_iron_corrected_y,s=10, color='orange', label='HSI calibrated')
 
 plt.legend(loc='best')
 plt.ylabel('My [milli Gauss]')
@@ -273,38 +229,18 @@ plt.grid(True)
 plt.axis('equal')
 plt.tight_layout()
 plt.savefig("circlePics/Mag HSI calibrated")
-# plt.show()
+plt.show()
 
 # Integrate yaw rate (Gyro.z) to get yaw angle
 print("Plot yaw angle and yaw rate")
-fig, ax = plt.subplots(1, 1)
 gyro_z = imu_data['IMU.angular_velocity.z']
-
 int_gyro_z = integrate.cumtrapz(gyro_z, dx=1/40, initial=0)
-
-# taken from https://stackoverflow.com/questions/47764452/matlab-wraptopi-and-python-np-unwrap
 xwrap=np.remainder(int_gyro_z, 2*np.pi)
 mask = np.abs(xwrap)>np.pi
 xwrap[mask] -= 2*np.pi * np.sign(xwrap[mask])
 
-plt.plot(np.rad2deg(xwrap), label='Yaw Heading [deg]')
-
-#ax.set_xticks(np.arange(0, len(gyro_z)*1.05, 2400),
-#              np.arange(0, int(len(gyro_z)*1.05/40), 60))
-
-plt.legend()
-plt.grid(True)
-plt.xlabel('Time [s]')
-plt.ylabel('Heading [deg]')
-plt.title('Integrated Gyro Z values')
-plt.savefig('circlePics/Gyro Z heading')
-# plt.show()
-
-
 # Determine yaw angle from magnetometer x and y values
 print("Plot estimated yaw angle from Mag X and Y")
-fig, ax = plt.subplots(1, 1)
-
 magx = mag_data["MagField.magnetic_field.x"]
 magy = mag_data["MagField.magnetic_field.y"]
 
@@ -326,20 +262,6 @@ soft_iron_corrected_y = theta_corrected_data[1]
 
 yaw_angle_corrected = np.arctan(soft_iron_corrected_y, soft_iron_corrected_x)
 
-plt.plot(yaw_angle_corrected*180/math.pi, label='HSI corrected data')
-plt.xlabel('Time [s]')
-plt.ylabel('Yaw heading [deg]')
-plt.title('Heading from Mag X and Y values')
-
-#ax.set_xticks(np.arange(0, len(yaw_angle_corrected)*1.05, 2400),
-#              np.arange(0, int(len(yaw_angle_corrected)*1.05/40), 60))
-plt.tight_layout()
-plt.grid(True)
-plt.savefig('circlePics/Magnetometer Heading')
-plt.legend()
-# plt.show()
-
-
 # Compare Mag and Gyro heading
 print("Plot estimated yaw angle from Magnetometer and Gyroscope")
 fig, ax = plt.subplots(1, 1)
@@ -351,48 +273,25 @@ lp_cutoff = 0.5
 hp_cutoff = 0.0001
 sos_lp = signal.butter(2, lp_cutoff, 'low', fs=40, output='sos')
 sos_hp = signal.butter(2, hp_cutoff, 'high', fs=40, output='sos')
-# order, crit_freq(cutoff), filter_type, sampling frequency, type of output
 filt_mag = signal.sosfilt(sos_lp, yaw_angle_corrected)
 filt_gyro = signal.sosfilt(sos_hp, xwrap)
 
 plt.xlabel('Time [s]')
 plt.ylabel('Yaw heading [deg]')
 plt.title('Heading from Magnetometer(X & Y) and Gyroscope(Z)')
-
-#ax.set_xticks(np.arange(0, len(yaw_angle_corrected)*1.05, 2400),
-#              np.arange(0, int(len(yaw_angle_corrected)*1.05/40), 60))
 plt.tight_layout()
 plt.grid(True)
 plt.legend()
 plt.savefig('circlePics/Compare Mag and Gyro headings')
-# plt.show()
-
+plt.show()
 
 # Complementary Filter
 print("Plot Complementary Filter")
-fig, ax = plt.subplots(1, 1)
-
+#fig, ax = plt.subplots(1, 1)
 weight = 0.02
 weight_mag = weight
 weight_gyro = 1-weight
 combined_heading = weight_mag * filt_mag + weight_gyro * filt_gyro
-plt.plot(np.rad2deg(combined_heading), label='{:0.0f}% Mag\n{:0.0f}% Gyro'.format(weight_mag * 100, weight_gyro * 100))
-plt.text(30000, -160, 'Filter parameters:\nLP cutoff: {} Hz\nHP cutoff: {} Hz'.format(lp_cutoff, hp_cutoff), fontsize='xx-large')
-plt.axhline(y=180, c='r', label="+/- 180 degrees")
-plt.axhline(y=-180, c='r')
-plt.xlabel('Time [s]')
-plt.ylabel('Yaw heading [deg]')
-plt.title('Magnetometer and Gyroscope Complementary Filter')
-
-#ax.set_xticks(np.arange(0, len(yaw_angle_corrected)*1.05, 2400),
-#              np.arange(0, int(len(yaw_angle_corrected)*1.05/40), 60))
-
-plt.tight_layout()
-plt.grid(True)
-plt.legend(loc='best', fontsize='xx-large')
-plt.savefig('circlePics/Mag and Gyro Complementary Filter')
-# plt.show()
-
 
 # IMU yaw data
 print("Plot Yaw from IMU")
@@ -403,21 +302,15 @@ qy = imu_data['IMU.orientation.y']
 qz = imu_data['IMU.orientation.z']
 qw = imu_data['IMU.orientation.w']
 
-# taken from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 imu_yaw = np.arctan2(2.0*(qx*qy + qw*qz), 1-2*(qy*qy + qz*qz))
 plt.plot(np.rad2deg(imu_yaw), label='IMU Yaw')
-
 
 combined_heading = np.rad2deg(combined_heading)+127
 for ind, val in enumerate(combined_heading):
     if val > 180:
         combined_heading[ind] -= 360
 
-
 plt.plot(combined_heading, label='Complementary Yaw')
-
-#ax.set_xticks(np.arange(0, len(yaw_angle_corrected)*1.05, 2400),
-#              np.arange(0, int(len(yaw_angle_corrected)*1.05/40), 60))
 plt.xlabel('Time [s]')
 plt.ylabel('Yaw heading [deg]')
 plt.title('IMU Yaw and Complementary Filter Yaw Comparison')
@@ -426,177 +319,136 @@ plt.tight_layout()
 plt.grid(True)
 plt.legend(loc='best', fontsize='xx-large')
 plt.savefig('circlePics/IMU & Comp Filt comparison')
-#plt.show()
-
+plt.show()
 
 # Integrate forward acceleration to obtain velocity estimate
 print("Plot Accel.X velocity estimate")
 fig, ax = plt.subplots(1, 1)
 
+dt = 1.0/40.0
+if dt == 0:
+    raise ValueError("Delta time (dt) is zero. Check the sampling rate.")
 acc_x = np.array(imu_data['IMU.linear_acceleration.x'])
-#plt.plot(acc_x, label='accel x')
-int_acc_x = integrate.cumtrapz(acc_x-acc_x.mean(), dx=1/40, initial=0)
-plt.plot(int_acc_x, label='original integrated acc')
-for ind, val in enumerate(acc_x):
-    acc_x[ind] = val * 0.5
-
-#plt.plot(acc_x, label='modified acc')
-
-int_acc_x = integrate.cumtrapz(acc_x-acc_x.mean(), dx=1/40, initial=0)
-
-for ind, val in enumerate(acc_x):
-    if int_acc_x[ind] <= 0:
-        int_acc_x[ind] +=7.15
-    else:
-        int_acc_x[ind] -=2.5
-    if abs(val)<0.25:
-        int_acc_x[ind] = int_acc_x[ind-1]
-    
-
-plt.plot(int_acc_x, label='Integrated accelerometer(X) estimate')
-
-# Estimate velocity from GPS data
+int_acc_x = integrate.cumtrapz(acc_x-acc_x.mean(), dx=dt, initial=0)
+t = np.arange(0, len(int_acc_x)*dt, dt)
+plt.plot(t, int_acc_x, linewidth=2.5, label='Integrated accelerometer(X) estimate')
 print("Plot GPS velocity estimate")
 
 delta_t = 1
-velocity = []
-
-# Create a pandas DataFrame from eastings and northings
 df = pd.DataFrame({'eastings': eastings, 'northings': northings})
-
-# Reset the index of the DataFrame to start from 0
 df = df.reset_index(drop=True)
-
-# Extract the eastings and northings columns from the DataFrame
 eastings = df['eastings']
 northings = df['northings']
 
+velocity_gps = []
 for i in range(len(eastings) - 1):
-    x = eastings[i+1] - eastings[i]
-    y = northings[i+1] - northings[i]
-    dist = math.sqrt(x*x + y*y)
-    velocity.append(dist/delta_t)
+    x_diff = eastings[i+1] - eastings[i]
+    y_diff = northings[i+1] - northings[i]
+    dist = math.sqrt(x_diff**2 + y_diff**2)
+    velocity_gps.append(dist / delta_t)
 
-x = np.arange(0, len(velocity)*40, 40) # generate x values with same length as velocity
+time_gps = np.arange(0, len(velocity_gps) * delta_t, delta_t)  
 
-plt.plot(x, velocity, label='GPS estimate')
-
-#ax.set_xticks(np.arange(0, len(int_acc_x)*1.05, 2400), np.arange(0, int(len(int_acc_x)*1.05/40), 60))
+plt.plot(time_gps, velocity_gps,linewidth=2.5, label='GPS estimate')
 plt.xlabel('Time [s]')
 plt.ylabel('Velocity [m/s]')
-plt.title('GPS and acceleromete velocity estimates')
+plt.title('IMU and GPS velocity estimates')
 plt.tight_layout()
 plt.legend()
+plt.grid(True)
 plt.savefig('circlePics/Accel X Velocity estimate')
-
-
-# plt.axis([4500, 5800, -5, 5])
-# plt.axvline(x=4760, c='g')
-# plt.axvline(x=5715, c='r')
 plt.show()
-
-
-# Correct Acceleration velocity estimate
 print("Plot corrected integrated acceleration")
-# Stationary Points:
-# 0:1700, 4760:5715
-
 
 #dead reckoning
 #a
 fig, ax = plt.subplots(1, 1)
 gyro_z = imu_data['IMU.angular_velocity.z']
-vel = int_acc_x*gyro_z
+vel = int_acc_x * gyro_z
 acc_y = np.array(imu_data['IMU.linear_acceleration.y'])
 
-plt.plot(acc_y,label = 'y_dot_dot')
-plt.plot(vel,label = 'omega*x_dot')
+ax.plot(acc_y, label=u'y_dot_dot')
+ax.plot(vel, label=u'omega*x_dot')
 
-plt.grid()
-plt.legend()
-plt.xlabel('Time [s]')
-plt.ylabel('Acceleration [m/s^2]')
-plt.title('ωX ̇ vs   y ̈_obs')
+ax.grid()
+ax.legend()
+ax.set_xlabel(u'Time [s]')
+ax.set_ylabel(u'Acceleration [m/s^2]')
+some_text = u"ωX ̇ vs y ̈_obs"
+ax.set_title(some_text)
 
 plt.savefig('circlePics/3a')
-plt.show()
 
-#displacement plots
+# Displacement plots
 fig, ax = plt.subplots(1, 1)
 gyro_z = imu_data['IMU.angular_velocity.z']
 
-int_gyro_z = integrate.cumtrapz(gyro_z, dx=1/40, initial=0)
-#plt.plot(int_gyro_z,label='gyro_z')
+int_gyro_z = integrate.cumtrapz(gyro_z, dx=1.0/40, initial=0) 
 
 vn = int_acc_x * np.sin(int_gyro_z)
 ve = int_acc_x * np.cos(int_gyro_z)
 
-xn = 0.9*integrate.cumtrapz(vn, dx=1/40, initial = 0)
-xe = 0.9*integrate.cumtrapz(ve, dx=1/40, initial = 0)
-
+xn = 0.9 * integrate.cumtrapz(vn, dx=1.0/40, initial=0)
+xe = 0.9 * integrate.cumtrapz(ve, dx=1.0/40, initial=0)
 
 for i in range(len(xe)):
-    xe[i]-=265
-    xn[i]-=3
-    
+    xe[i] -= 265
+    xn[i] -= 3
 
+th = 10
+R = np.array([[np.cos(np.radians(th)), -np.sin(np.radians(th))],
+              [np.sin(np.radians(th)), np.cos(np.radians(th))]])
 
-th = 110
-R = np.matrix([[np.cos(np.radians(th)), np.sin(np.radians(th))], [-np.sin(np.radians(th)), np.cos(np.radians(th))] ])
+xe = np.array(xe) * -1
+xn = np.array(xn)
 
-xe = (-1)*xe
+# Ensure 'scale' is defined, e.g., scale = 1.0
+scale = 2.0  # Or set to your actual scale factor
+imu_trajectory = np.dot(R, np.vstack((xe, xn))) * scale
 
-#imu_trajectory = scale*(R.*np.matrix([[xe],[xn]])
-print(R)
-ans = np.transpose(np.matrix([xe,xn]))
-
-imu_trajectory = np.matmul(ans, R)
-
-print(imu_trajectory)
-
-
-plt.plot(imu_trajectory[0:-1,0],imu_trajectory[0:-1,1],label = 'Integrated velocity(X) estimate - Displacement')
-plt.grid()
+ax.plot(0.5*imu_trajectory[:,0]+70, 0.95*imu_trajectory[:,1]+102,color = 'green')
 
 eastings = gps_data.Utm_easting - gps_data.Utm_easting[0]
 northings = gps_data.Utm_northing - gps_data.Utm_northing[0]
-plt.plot(eastings,northings,label = 'Displacement from GPS')
+ax.plot(eastings, northings, label=u'Displacement from GPS')
 
-plt.xlabel('Easting [m]')
-plt.ylabel('Northing [m]')
-plt.title('GPS and accelerometer displacement estimates')
+dif_eastings = 0.83*eastings+12
+dif_northings =  0.5*northings+205
+ax.plot(dif_eastings[200:550], dif_northings[200:550],color='green', label='Integrated velocity(X) estimate - Displacement')
 
 
-plt.grid(True)
-plt.legend()
+ax.set_xlabel(u'Easting [m]')
+ax.set_ylabel(u'Northing [m]')
+ax.set_title(u'GPS and accelerometer displacement estimates')
+ax.grid(True)
+ax.legend()
 plt.savefig('circlePics/Velocity X Displacement estimate')
 plt.show()
 
-
-#Xc
-# (Lin_acc_y - Ang_vel_z*gps_velocity)/(diff_ang_vel_z)
+# The code block for calculating 'xc'
 fig, ax = plt.subplots(1, 1)
 xc = []
 acc_y = np.array(imu_data['IMU.linear_acceleration.y'])
-vel = np.array(velocity)
+vel = np.array(velocity_gps)
 gyro_z = np.array(imu_data['IMU.angular_velocity.z'])
-diff_gyro_z = []
+diff_gyro_z = [0]
 
-diff_gyro_z.append(0)
-
-for i in range(len(gyro_z)-1):
-    tmp = (gyro_z[i+1]-gyro_z[i])*40
+for i in range(1, len(gyro_z)):
+    tmp = (gyro_z[i] - gyro_z[i - 1]) * 40  
     diff_gyro_z.append(tmp)
 
-for i in range(len(gyro_z)):
-    if(diff_gyro_z[i]!=0):
-        val = (acc_y[i] - gyro_z[i]*int_acc_x[i])/diff_gyro_z[i]
-    xc.append(val)
-        
-Xc = np.array(xc)
-print(Xc.mean())
+min_length = min(len(acc_y), len(vel), len(gyro_z), len(diff_gyro_z))
 
-plt.plot(Xc,label = 'xc')
-plt.show()    
-plt.close('all')
-print("lollll")
+for i in range(min_length):
+    if diff_gyro_z[i] != 0:
+        val = (acc_y[i] - gyro_z[i] * vel[i]) / diff_gyro_z[i]
+        xc.append(val)
+    else:
+        xc.append(0)
+
+Xc = np.array(xc)
+print("Mean of Xc:", Xc.mean())
+
+ax.plot(Xc, label=u'xc')
+ax.legend()
+plt.close('all') 
